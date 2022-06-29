a1ed6814b4f3ae0329c9248acb42ceed17eebeb0
Ian <vraxjs@gmail.com>
2022-06-29 11:34:45 -0500

    ASD062922

--- Js/register.js
+++ Js/register.js
@@ -2,6 +2,16 @@ const $UserName = document.getElementById('UserID');
 const $Password = document.getElementById('PasswordID');
 const $Button = document.getElementById('btn');
 const $Alert = document.getElementById('alert');
+const $PerritoImg = document.querySelector('img');
 const $Error = 'DB IS NOT CONNECTED.';
 
-const hello = "BORRAR";
+
+
+$PerritoImg.addEventListener('click', ()=>{
+    let mySrc = $PerritoImg.getAttribute('src');
+    if (mySrc === '/Hachiko2022.jpg') {
+        $PerritoImg.setAttribute('src', '/PerritoNoPanzon.jpg');
+    } else{
+        $PerritoImg.setAttribute('src', '/Hachiko2022.jpg');
+    }
+});
