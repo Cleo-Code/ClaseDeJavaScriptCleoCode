4d57e919a04226202ef2cf5dce92648d8ca34aca
Ian <vraxjs@gmail.com>
2022-06-29 07:44:59 -0500

    register Javascript Clean

--- Js/register.js
+++ Js/register.js
@@ -4,19 +4,4 @@ const $Button = document.getElementById('btn');
 const $Alert = document.getElementById('alert');
 const $Error = 'DB IS NOT CONNECTED.';
 
-$Button.addEventListener('click', ()=>{
-
-    const _setInfo = ()=>{
-        $Alert.style.display = 'block';
-        $Alert.style.color = 'yellow';
-        $Alert.textContent = 'Verifyng...';
-    };
-
-    const _setUser = ()=>{
-        localStorage.setItem('UserName', $UserName.value);
-        alert('se guardo en la BD: ' + localStorage.getItem('UserName'))
-    };
-
-    _setUser()
-});
 
